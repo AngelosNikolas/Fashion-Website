6b9c915ba563aa9efc668d660e3c78ff43690460
william <42318592+backendprogrammer@users.noreply.github.com>
2021-04-28 21:22:37 +0100

    Update addProduct.php

--- Editor/addProduct.php
+++ Editor/addProduct.php
@@ -1,3 +1,39 @@
+<?php
+session_start();
+
+require_once('config.php');
+
+if (isset($_SESSION['logged-in'])) {
+    $email = $_SESSION['email'];
+    $sql = "select * from user where userName='$email'";
+    $query = mysqli_query($con, $sql);
+    $res = mysqli_num_rows($query);
+
+    if ($res > 0) {
+        while ($row = mysqli_fetch_assoc($query)) {
+            $_SESSION['status'] = $row['status'];
+            $status = $_SESSION['status'];
+        }
+        if($status === 0){
+            echo "<script>
+window.location.href='../Front-end/index.php';
+</script>";
+        }
+
+    }
+}
+
+if (!isset($_SESSION["logged-in"])) {
+    echo "<script>
+window.location.href='../Front-end/index.php';
+</script>";
+
+    exit();
+}
+
+
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -35,31 +71,39 @@
 </head>
 
 <body style="font-family: Oswald, sans-serif;background: url(&quot;assets/img/lines%20backround%202%20dark.jpg&quot;);">
-    <nav class="navbar navbar-light navbar-expand-md navigation-clean-button" id="navclass" style="color: var(--gray-dark);background: rgba(255,255,255,0.62);">
-        <div class="container"><a class="navbar-brand" href="#">Company Name</a><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
-            <div class="collapse navbar-collapse" id="navcol-1">
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-item"><a class="nav-link" href="addProduct.php">Add Product</a></li>
-                    <li class="nav-item"><a class="nav-link" href="searchProduct.php">Search Product</a></li>
-                </ul>
-            </div>
-        </div>
-		<div class="dropdown">
-		<a class="dropdown-toggle" aria-expanded="false" data-toggle="dropdown" href="#" style="color: #434a52;">Account</a>
-                <div class="dropdown-menu">
-				<?php
-                if (isset($_SESSION['logged-in'])) {
-                    echo"<a class=\"dropdown-item\" data-bss-hover-animate=\"pulse\" href=\"signOut.php\" style=\"color: rgb(0,0,0);background: #e87fc9;\">Sign out</a></div>";
+    <div><nav class="navbar navbar-light navbar-expand-md navigation-clean-search" id="navclass" style="color: var(--gray-dark);background: rgba(255,255,255,0.62);">
+            <div class="container"><a class="navbar-brand" href="../Front-end/index.php" style="background: url(&quot;assets/img/logoFinal.png;) center / contain no-repeat;height: 53px;width: 120.8281px;"></a><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navcol-1" style="color: var(--warning);">
+                    <ul class="navbar-nav">
+                        <li class="nav-item"><a class="nav-link" href="addProduct.php">Add Product</a></li>
+                        <li class="nav-item"><a class="nav-link" href="searchProduct.php">Search Product</a></li>
 
-                } else{
-                    (empty($_SESSION['logged-in']));
-                    echo "<a class=\"dropdown-item\" data-bss-hover-animate=\"pulse\" href=\"signForm.php\" style=\"color: rgb(0,0,0);background: #e5c6db;\">Sign up</a>
-                    <a class=\"dropdown-item\" data-bss-hover-animate=\"pulse\" href=\"login.php\" style=\"color: rgb(0,0,0);background: #e87fc9;\">Log in</a></div>";
-                }
-                ?>
-				
-            </div>
-    </nav>
+                    </ul>
+                </div>
+                <ul class="navbar-nav">
+                    <li class="nav-item"></li>
+
+                    <li class="nav-item"><a class="nav-link" href="../Front-end/profile.php"><i class="fa fa-user"></i></a></li>
+                    <li class="nav-item"></li>
+                </ul>
+                <div class="dropdown"><a class="dropdown-toggle" aria-expanded="false" data-toggle="dropdown" href="#" style="color: #434a52;">Account</a>
+                    <div class="dropdown-menu">
+                        <?php
+                        if (isset($_SESSION['logged-in'])) {
+                            echo"<a class=\"dropdown-item\" data-bss-hover-animate=\"pulse\" href=\"../Front-end/signOut.php\" style=\"color: rgb(0,0,0);background: #e87fc9;\">Sign out</a></div>";
+
+                        } else{
+                            (empty($_SESSION['logged-in']));
+                            echo "<a class=\"dropdown-item\" data-bss-hover-animate=\"pulse\" href=\"../Front-end/signForm.php\" style=\"color: rgb(0,0,0);background: #e5c6db;\">Sign up</a>
+                    <a class=\"dropdown-item\" data-bss-hover-animate=\"pulse\" href=\"../Front-end/login.php\" style=\"color: rgb(0,0,0);background: #e87fc9;\">Log in</a>";
+                        }
+                        ?>
+                    </div>
+                </div>
+        </nav>
+
+
+    </div>
     <section class="contact-clean">
         <form class="bootstrap-form-with-validation" action="addProductProcess.php" method="post" method="post"enctype="multipart/form-data">
             <h2 class="text-center">Add Product</h2>
@@ -127,3 +171,4 @@ el.value = parseFloat(elValue).toFixed(2);
 
 }
 </script>
+
