8ad99bfed6f53cf2c78304be45806e20bbc7f2c5
AngelosNikolas <79995518+AngelosNikolas@users.noreply.github.com>
2021-04-29 12:11:38 +0100

    Update login.php
    changed old version with new

--- Front-end/login.php
+++ Front-end/login.php
@@ -27,26 +27,22 @@ session_start();
 </head>
 
 <body>
-    <nav class="navbar navbar-light navbar-expand-md navigation-clean-search" style="color: rgb(232,127,201);background: var(--white);">
-        <div class="container"><a class="navbar-brand" href="index.php" style="background: url(&quot;assets/img/logoFinal.png&quot;) center / contain no-repeat;height: 53px;width: 120.8281px;"></a><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+    <nav class="navbar navbar-light navbar-expand-md navigation-clean-search" style="color: rgb(232,127,201);background: #e5c6db;">
+        <div class="container"><a class="navbar-brand" href="index.php" style="background: url(&quot;assets/img/logoFinal.png&quot;) center / contain no-repeat;height: 53px;width: 120.8281px;"></a></div>
     </nav>
-    <section class="login-dark" style="background: url(&quot;../Front-end/assets/img/friends.jpg&quot;) center / cover;">
-        <form action="loginScript.php" method="post">
+    <section class="login-dark" style="background: url(&quot;assets/img/friends.jpg&quot;) center / cover;">
+        <form method="post">
             <h2 class="sr-only">Login Form</h2>
-            <div class="illustration"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 20 20" fill="none" style="border-color: var(--blue);color: #333333;">
-
-                    <path fill-rule="evenodd" clip-rule="evenodd" d="M10 9C11.6569 9 13 7.65685 13 6C13 4.34315 11.6569 3 10 3C8.34315 3 7 4.34315 7 6C7 7.65685 8.34315 9 10 9ZM3 18C3 14.134 6.13401 11 10 11C13.866 11 17 14.134 17 18H3Z" fill="currentColor"></path>
-                </svg></div>
-            <div class="form-group"><input class="form-control" type="email" name="email" placeholder="Email"></div>
-            <div class="form-group"><input class="form-control" type="password" name="password" placeholder="Password"></div>
-            <div class="form-group"><button class="btn btn-primary btn-block" name="submit" type="submit" style="background: #434a52;">Log In</button></div><a class="forgot" href="#">Forgot your email or password?</a>
+            <div><img src="assets/img/logoFinal.png" style="width: 239px;height: 167.359px;"></div>
+            <div class="form-group"><input class="form-control" type="email" name="email" placeholder="Email" style="color: #2b2b28;font-family: Oswald, sans-serif;"></div>
+            <div class="form-group"><input class="form-control" type="password" name="password" placeholder="Password" style="font-family: Oswald, sans-serif;color: #2b2b28;"></div>
+            <div class="form-group"><button class="btn btn-primary btn-block" type="submit" style="background: #434a52;">Log In</button></div><a class="forgot" href="#">Forgot your email or password?</a>
         </form>
     </section>
     <script src="assets/js/jquery.min.js"></script>
     <script src="assets/bootstrap/js/bootstrap.min.js"></script>
     <script src="assets/js/bs-init.js"></script>
     <script src="assets/js/Profile-Edit-Form.js"></script>
-    <script src="assets/js/script.js"></script>
 </body>
 
 </html>
